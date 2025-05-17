2d4df5dddb51d9d1e2ca30f0a0832344c524bffa
skunks13 <36896017+skunks13@users.noreply.github.com>
2025-05-17 20:11:30 +0300

    Update 17_openvas.sh

--- scripts/17_openvas.sh
+++ scripts/17_openvas.sh
@@ -20,6 +20,6 @@ cargo build --release
 cd $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON/rust/src/scannerctl
 cargo build --release
 
-sudo cp -v ../target/release/openvasd $INSTALL_DIR/openvasd/usr/local/bin/
-sudo cp -v ../target/release/scannerctl $INSTALL_DIR/openvasd/usr/local/bin/
+sudo cp -v $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON/rust/target/release/openvasd $INSTALL_DIR/openvasd/usr/local/bin/
+sudo cp -v $SOURCE_DIR/openvas-scanner-$OPENVAS_DAEMON/rust/target/release/scannerctl $INSTALL_DIR/openvasd/usr/local/bin/
 sudo cp -rv $INSTALL_DIR/openvasd/* /
