27c3d3fec92818d5bde9844aac4a72f32a044d44
skunks13 <36896017+skunks13@users.noreply.github.com>
2025-06-16 16:36:21 +0300

    Update update_web.sh

--- update_web.sh
+++ update_web.sh
@@ -2,10 +2,12 @@
 #
 source ./exports.sh
 
-curl -f -L https://github.com/skunks13/gsa/releases/download/v$GSA_VERSION/gsa-dist-$GSA_VERSION.tar.gz -o $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+export LSA_VERSION=24.3.0
 
-mkdir -p $SOURCE_DIR/gsa-$GSA_VERSION
-tar -C $SOURCE_DIR/gsa-$GSA_VERSION -xvzf $SOURCE_DIR/gsa-$GSA_VERSION.tar.gz
+curl -f -L https://github.com/skunks13/lsa/releases/download/v$LSA_VERSION/gsa-dist-$LSA_VERSION.tar.gz -o $SOURCE_DIR/gsa-$LSA_VERSION.tar.gz
+
+mkdir -p $SOURCE_DIR/gsa-$LSA_VERSION
+tar -C $SOURCE_DIR/gsa-$LSA_VERSION -xvzf $SOURCE_DIR/gsa-$LSA_VERSION.tar.gz
 
 sudo mkdir -p $INSTALL_PREFIX/share/gvm/gsad/web/
-sudo cp -rv $SOURCE_DIR/gsa-$GSA_VERSION/* $INSTALL_PREFIX/share/gvm/gsad/web/
+sudo cp -rv $SOURCE_DIR/gsa-$LSA_VERSION/* $INSTALL_PREFIX/share/gvm/gsad/web/
